c2c1c5142812444e03c5e76b76c9833e10570d54
dovczitter <dov_czitter@yahoo.com>
2023-09-01 12:52:36 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,309 @@
+from kivymd.app import MDApp
+from kivy.clock import Clock
+from kivymd.uix.boxlayout import MDBoxLayout
+
+from kivy.uix.scrollview import ScrollView
+from kivymd.uix.list import MDList, OneLineListItem
+
+from kivy.uix.floatlayout import FloatLayout
+from kivy.factory import Factory
+from kivy.properties import ObjectProperty
+from kivy.uix.popup import Popup
+from kivy.utils import platform
+
+import time
+
+from os.path import basename,exists
+from jnius import autoclass
+
+from recorder import Recorder
+
+mp4Recorder = ''
+loadFilename = ''
+emailFileMsg = ''
+      
+# ============================================
+#               Mp4Recorder
+# ============================================
+class Mp4Recorder(MDBoxLayout):
+    
+    def __init__(self, **kwargs):
+        
+        if not platform == "android":
+            # ===== ToDo: Log an error =========
+            return
+
+        from android.permissions import request_permissions, Permission
+        
+        global mp4Recorder
+        
+        super(Mp4Recorder, self).__init__(**kwargs)
+        #
+        # 'request_permissions' first, waits in background until 'permissions_external_storage'
+        # is complete. Then allows for the request respons, then continues with kivy. 
+        print('========== BEFORE init request_permissions ==============')
+        request_permissions([Permission.RECORD_AUDIO, Permission.ACCESS_WIFI_STATE, Permission.INTERNET])
+        print('========== AFTER  init request_permissions ==============')
+      
+        # https://programtalk.com/vs4/python/adywizard/car-locator/main.py/
+        # https://github.com/kivy/plyer/issues/661
+        print('========== BEFORE init permissions_external_storage ==============')
+        self.permissions_external_storage()
+        print('========== AFTER  init permissions_external_storage ==============')
+
+        mp4Recorder = Recorder()
+
+        self.state = 'ready'
+        self.time_started = False
+        
+        # ------------ ScrollView stuff. ie 'Info' -----------------
+        self.sv = ScrollView()
+        self.ml = MDList()
+        self.sv.add_widget(self.ml)
+        self.contacts = []
+                     
+        self.file_choose_root = Root()
+        
+        rsp = self.wifiCheck()
+        print(f'after self.wifiCheck(), [{rsp}]')
+        
+        self.start_time()
+
+    def permissions_external_storage(self, *args):  
+        # https://github.com/kivy/plyer/issues/661                
+        if platform == "android":
+            PythonActivity = autoclass("org.kivy.android.PythonActivity")
+            Environment = autoclass("android.os.Environment")
+            Intent = autoclass("android.content.Intent")
+            Settings = autoclass("android.provider.Settings")
+            Uri = autoclass("android.net.Uri")
+            # If you have access to the external storage, do whatever you need
+            if Environment.isExternalStorageManager():
+                # If you don't have access, launch a new activity to show the user the system's dialog
+                # to allow access to the external storage
+                pass
+            else:
+                try:
+                    from typing import cast
+                    activity = PythonActivity.mActivity.getApplicationContext()
+                    uri = Uri.parse("package:" + activity.getPackageName())
+                    intent = Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION, uri)
+                    currentActivity = cast(
+                    "android.app.Activity", PythonActivity.mActivity
+                    )
+                    currentActivity.startActivityForResult(intent, 101)
+                except:
+                    intent = Intent()
+                    intent.setAction(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION)
+                    currentActivity = cast(
+                    "android.app.Activity", PythonActivity.mActivity
+                    )
+                    currentActivity.startActivityForResult(intent, 101) 
+    #
+    # -------- timer -------
+    #
+    def timer(self, *args):
+        global loadFilename
+        global emailFileMsg
+
+        time_str = f'Mp4Recorder [{time.asctime()}]'
+        chk = ''
+        # -------- TODO - log transition ----------- 
+        if self.wifiCheck():
+            chk = '* UP *'
+            self.ids.time_label.color = "orange"
+        else:
+            chk = '- DN -'
+            self.ids.time_label.color = "yellow"
+
+        wifi_str = f'Wifi {chk}'
+        self.ids.time_label.text = f'{time_str} - [{wifi_str}]'
+        
+        if exists(loadFilename):
+            self.update_labels()
+    #
+    # -------- start_time -------
+    #
+    def start_time(self):
+        Clock.schedule_interval(self.timer, 1)
+
+    #
+    # -------- wifiCheck -------
+    #
+    def wifiCheck(self):
+        from ping3 import ping
+        # https://github.com/kyan001/ping3
+        # UP rsp : 0.016164541244506836
+        # DN rsp : None
+        rsp = ping('google.com')
+        return isinstance(rsp, float)
+    #
+    # -------- LogMessage ------- WIP -----
+    #
+#    @staticmethod
+    def LogMessage(self, msg):
+        from datetime import datetime
+        
+        now = datetime.now()
+        dt_string = now.strftime("%d%b%Y_%H%M%S")
+        
+        logmsg = f'[{dt_string}] {msg}'
+
+        self.ids.container.add_widget(
+            OneLineListItem(text=logmsg)
+        )
+        self.sv.scroll_to(logmsg)
+    
+    # ======================
+    #       record 
+    # ======================
+    def record(self):
+        global mp4Recorder
+        self.state = mp4Recorder.record(self.state)
+        self.update_labels()
+
+    # ======================
+    #       email 
+    # ======================
+    def email(self):
+        global mp4Recorder
+
+        msg = ''
+        if self.state != 'ready':
+            msg = 'Recording in progress.'
+        else:
+            recordFilename = mp4Recorder.get_mp4_filename()
+            print(f'================= recordFilename [{recordFilename}] ====================')
+            msg = mp4Recorder.email(recordFilename)
+        
+        self.LogMessage(msg)
+        
+        self.update_labels()
+
+    # ======================
+    #       emailfile 
+    # ======================
+    def emailfile(self):
+        if self.state != 'ready':
+            self.LogMessage('Recording in progress.')
+            self.update_labels()
+            return
+        
+        # ------------------------------------------------------------------------
+        # Root emailfile class will do the actual filechoose and email.
+        # See show_load() stuff.
+        # 'loadFilename' reported in timer() via update_labels() when available.
+        # ------------------------------------------------------------------------
+        self.file_choose_root.show_load()
+            
+        self.update_labels()
+        
+    # ======================
+    #       exit 
+    # ======================
+    def exit(self):
+        global mp4Recorder
+        mp4Recorder.exit()
+
+    # ======================
+    #       update_labels
+    # ======================
+    def update_labels(self):
+        global mp4Recorder
+        global loadFilename
+
+        # --------- Button label updates --------
+        if self.state == 'ready':
+            self.ids.record_button.text = 'START Recording'
+
+        if self.state == 'recording':
+            self.ids.record_button.text = 'STOP Recording'
+
+        # -------- Email and EmailFile updates
+        recordFilename = mp4Recorder.get_mp4_filename()
+        if exists(recordFilename):
+            basefn = basename(recordFilename)
+            end_msg = f'[Audio : {self.state}] [Recorded File : {basefn}] '
+            self.LogMessage(end_msg)
+
+        if exists(loadFilename):
+            basefn = basename(loadFilename)
+            end_msg = f'[Email File : {basefn}] '
+            self.LogMessage(end_msg)
+            # NOTE - clear loacal loadFilename, generates only one update per load.
+            loadFilename = ''
+            
+# ============================================
+#               LoadDialog
+# ============================================
+class LoadDialog(FloatLayout):
+    
+    def sort_by_date(files, filesystem):    
+        import os
+        return (sorted(f for f in files if filesystem.is_dir(f)) +
+            sorted((f for f in files if not filesystem.is_dir(f)), key=lambda fi: os.stat(fi).st_mtime, reverse = True))
+        
+    def sort_by_name(files, filesystem):
+        return (sorted(f for f in files if filesystem.is_dir(f)) +
+                sorted(f for f in files if not filesystem.is_dir(f))) 
+        
+    default_sort_func = ObjectProperty(sort_by_date)
+          
+    emailfile = ObjectProperty(None)
+    cancel = ObjectProperty(None)
+    sort = ObjectProperty(None)
+    
+# ============================================
+#               Root
+# ============================================
+class Root(FloatLayout):
+
+    #
+    # -------- dismiss_popup --------
+    #
+    def dismiss_popup(self):
+        self._popup.dismiss()
+
+    #
+    # -------- show_popup --------
+    #
+    def show_load(self):
+        global mp4Recorder
+        
+        content = LoadDialog(emailfile=self.emailfile, cancel=self.dismiss_popup)
+        print(f'=========== show_load path [{mp4Recorder.get_mp4_path()}] ==========')
+        content.ids.filechooser.path = mp4Recorder.get_mp4_path()
+
+        self._popup = Popup(title="Load file", content=content, size_hint=(0.9, 0.9))
+        self._popup.open()
+ 
+    #
+    # -------- emailfile --------
+    #
+    def emailfile(self, path, selection):
+        global mp4Recorder
+        global loadFilename
+        global emailFileMsg
+        msg = ''
+        try:
+            loadFilename = selection[0]
+            if exists(loadFilename):
+                emailFileMsg = mp4Recorder.email(loadFilename)
+            else:
+                emailFileMsg = f'Email File error, file [{loadFilename}] does not exist'
+            self.dismiss_popup()
+        except:
+            pass
+        
+class Mp4RecorderApp(MDApp):
+    def build(self):
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = "Orange"
+        return Mp4Recorder()
+
+Factory.register('Root', cls=Root)
+Factory.register('LoadDialog', cls=LoadDialog)
+
+if __name__ == '__main__':
+    
+    Mp4RecorderApp().run()
---
+++ recorder.py
@@ -0,0 +1,291 @@
+from jnius import autoclass
+from kivy import Logger, platform
+from datetime import datetime
+import os
+import traceback
+
+from sharedstorage import SharedStorage
+
+#from moviepy.editor import VideoFileClip
+
+#
+Environment = None
+
+if platform == "android":
+    from android import autoclass
+    Environment = autoclass('android.os.Environment')
+
+# =========================================================== #
+#                   class Recorder                         #
+# =========================================================== #
+
+class Recorder():
+
+# https://developer.android.com/reference/android/media/MediaRecorder.OutputFormat
+# https://stackoverflow.com/questions/4886365/androidhow-to-get-media-recorder-output-in-mp3-format
+# https://pypi.org/project/PythonVideoConverter/
+# https://dev.to/dheerajprogrammer/mp4-to-mp3-converter-in-python-5eba
+
+# This one looks the best....
+# https://webcodespace.com/how-to-convert-mp4-to-mp3-using-python/
+
+    def __init__(self, **kwargs):
+        super(Recorder, self).__init__(**kwargs)
+
+        if not platform == "android":
+            return
+        
+        self.MediaRecorder = autoclass('android.media.MediaRecorder')
+        self.AudioSource = autoclass('android.media.MediaRecorder$AudioSource')
+        self.OutputFormat = autoclass('android.media.MediaRecorder$OutputFormat')
+        self.AudioEncoder = autoclass('android.media.MediaRecorder$AudioEncoder')
+        #
+        self.mp4_filename = ''
+        self.mp4Fn = ''
+        self.config = dict()
+        self.BASE_FILENAME = ''
+        self.EMAIL_USERNAME = ''
+        self.EMAIL_PASSWORD = ''
+        self.EMAIL_FROM = ''
+        self.EMAIL_TO = ''
+        self.SERVER_HOST = ''
+        self.SERVER_PORT = 0
+        
+        self.configInit()
+            
+    # ----------------- configInit ------------------------ #        
+    def configInit(self):
+        
+        from os.path import exists
+        import shutil
+#       from Mp3Recorder import LogMessage
+        
+        self.config.clear()
+            
+        print('START # ----------------- configInit ------------------------ #')
+        # ==============================================================================================
+        # Note for local editing Mp3Recorder.cvs file [problems due to file extension and write restrictions in 'Documents']:
+        # Name the new local edited file 'Mp3Recorder.apk' [or '.txt', not '.csv']
+        # Copy via explorer 'Mp3Recorder.apk' to the Documents folder
+        # Delete the old 'Mp3Recorder.csv' [or rename to something with a '.csv' ext, ie 'Mp3RecorerOld.csv']
+        # Rename the new 'Mp3Recorder.apk' file to 'Mp3Recorder.csv', now you're done.
+        # ==============================================================================================
+        SharePath = './Mp4Recorder.csv'
+        ConfigPath = '/storage/emulated/0/Documents/Mp4Recorder/Mp4Recorder.csv' 
+        path = ''  
+        content_list = []    
+        try:
+            
+            # Move a local copy and read config from here, may be locally modified.
+            # https://www.tutorialspoint.com/How-to-copy-files-from-one-folder-to-another-using-Python
+            documents_dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS).getPath()
+            ConfigPath = f'{documents_dir}/Mp4Recorder/Mp4Recorder.csv'
+            if exists(ConfigPath) == False:
+                ss = SharedStorage()
+                share = ss.copy_to_shared(SharePath)
+                path = ss.copy_from_shared(share)
+                
+            with open(ConfigPath) as f:
+                content_list = f.readlines()
+            f.close()
+
+            for item in content_list:
+                i = item.replace('\n','')
+                print(f'[{i}]')
+                itemList = []
+                try:
+                    itemList = item.replace(' ','').split(',')
+                except ValueError:
+                    continue
+                if len(itemList) > 1 and itemList[0][0] != '#':
+                    # cleanup NL etc
+                    for i,s in enumerate(itemList):
+                        itemList[i] = s.strip()
+                    k = itemList[0].strip()
+                    v = itemList[1:]
+                    self.config[k] = v
+                    print(f'[ {k}:{v} ]')
+                    
+            if self.config['BaseFilename']: 
+                self.BASE_FILENAME = str(self.config['BaseFilename'][0]).replace(' ','')
+                print(f'self.BASE_FILENAME: {self.BASE_FILENAME}')
+            if self.config['Username']:
+                self.EMAIL_USERNAME = str(self.config['Username'][0]).replace(' ','')
+                print(f'self.EMAIL_USERNAME: {self.EMAIL_USERNAME}')
+            if self.config['Password']:
+                self.EMAIL_PASSWORD = str(self.config['Password'][0]).replace(' ','')
+                print(f'self.EMAIL_PASSWORD: {self.EMAIL_PASSWORD}')
+            if self.config['From']:
+                self.EMAIL_FROM = str(self.config['From'][0]).replace(' ','')
+                print(f'self.EMAIL_FROM: {self.EMAIL_FROM}')
+            if self.config['To']:
+                lst = self.config['To']
+                self.EMAIL_TO = []
+                for s in lst:
+                    s2 = s.replace(' ','')
+                    if len(s2) > 0:
+                        self.EMAIL_TO.append(s2)
+                print(f'self.EMAIL_TO: {self.EMAIL_TO}')
+            if self.config['Host']:
+                self.SERVER_HOST = str(self.config['Host'][0]).replace(' ','')
+                print(f'self.SERVER_HOST: {self.SERVER_HOST}')
+            if self.config['Port']:
+                self.SERVER_PORT = int(str(self.config['Port'][0]).replace(' ',''))
+                print(f'self.SERVER_PORT: {self.SERVER_PORT}')
+               
+#            LogMessage(f'Receiver: {self.EMAIL_TO}')
+#            rcvr = self.config['Receiver'] 
+#            LogMessage(f'self.config[Receiver]: {rcvr}') 
+                
+        except Exception as error:
+            self.config.clear()
+            err = traceback.format_exc()
+            print(' ================================ configInit traceback START ================================ ')
+            print(err)
+            print(' ================================ configInit traceback END ================================ ')
+            
+    def create_recorder(self):
+        now = datetime.now()
+        dt_string = now.strftime("%d%b%Y_%H%M%S")
+        self.mp4Fn = f'{self.BASE_FILENAME}_{dt_string}.mp4'
+        
+        self.recorder = self.MediaRecorder()
+        self.recorder.setAudioSource(self.AudioSource.MIC)
+        self.recorder.setOutputFormat(self.OutputFormat.MPEG_4)
+        self.recorder.setOutputFile(self.mp4Fn)
+        self.recorder.setAudioEncoder(self.AudioEncoder.AAC)
+        self.recorder.prepare()
+
+    def get_recorder(self):
+        if not hasattr(self, "recorder"):
+            self.create_recorder()
+        return self.recorder
+
+    def remove_recorder(self):
+        delattr(self, "recorder")
+   
+    # ----------------- record_start ------------------------ #
+    def record_start(self):
+        self.get_recorder()
+        self.mp4_filename = ''
+        self.create_recorder()
+        self.recorder.start()
+    
+    # ----------------- record_stop ------------------------ #
+    def record_stop(self):
+        self.get_recorder()
+        self.recorder.stop() 
+      
+        # Copy the mp4 to Main Storage:
+        ss = SharedStorage()
+        share = ss.copy_to_shared(self.mp4Fn)
+        path = ss.copy_from_shared(share)
+        
+        self.mp4_filename = path
+
+        self.recorder.reset()
+        self.recorder.release()
+        # we need to do this in order to make the object reusable
+        self.remove_recorder()
+
+    # ----------------- get_mp4_filename ------------------------ #
+    def get_mp4_filename(self):
+        return self.mp4_filename
+
+    # ----------------- get_mp4_path ------------------------ #
+    def get_mp4_path(self):
+        path = "/storage/emulated/0/Movies/Mp4Recorder"
+        return path
+    
+    # ======================
+    #       record 
+    # ======================
+    def record(self,state):
+        if state == 'ready':
+            state = 'recording'
+            if len(self.config) == 0:
+                self.configInit()
+            self.record_start()
+        elif state == 'recording':
+            state = 'ready'
+            self.record_stop()
+        return state
+
+    # ======================
+    #       email 
+    # ======================
+
+    def send_email(self, filename):
+        import smtplib
+        from email.mime.multipart import MIMEMultipart
+        from email.mime.text import MIMEText
+        from email.mime.base import MIMEBase
+        from email import encoders
+        from os.path import exists
+        import traceback
+
+        if filename == None or not exists(filename):
+            return f' Error: [{filename}] does not exist.'
+        
+        basefn = os.path.basename(filename)
+        
+        # set up the SMTP server
+        server = smtplib.SMTP(self.SERVER_HOST, self.SERVER_PORT)
+        server.ehlo()
+        server.starttls()
+        server.ehlo()
+        
+        print(f'SERVER_HOST |{self.SERVER_HOST}|')
+        print(f'SERVER_PORT |{self.SERVER_PORT}|')
+        print(f'EMAIL_USERNAME |{self.EMAIL_USERNAME}|')
+        print(f'EMAIL_PASSWORD |{self.EMAIL_PASSWORD}|')
+        print(f'EMAIL_TO |{self.EMAIL_TO}|')
+        
+        server.login(self.EMAIL_USERNAME, self.EMAIL_PASSWORD)
+        
+        # setup payload
+        msg = MIMEMultipart()
+        msg['From'] = self.EMAIL_FROM
+        msg['To'] = ",".join(self.EMAIL_TO)
+
+        # Subject
+        msg['Subject'] = f'[{basefn}] From Mp4Recorder'
+
+        # Body
+        body = f'[{basefn}] From Mp4Recorder'
+        msg.attach(MIMEText(body, 'plain'))
+
+        # file attachment
+        attachment = open(filename, "rb")
+        p = MIMEBase('application', 'octet-stream')
+        p.set_payload((attachment).read())
+        encoders.encode_base64(p)
+        p.add_header('Content-Disposition', "attachment; filename= %s" % basefn)
+        msg.attach(p)
+        
+        #Send the mail
+        # Note max emails per day limit:
+        # https://answers.microsoft.com/en-us/outlook_com/forum/all/smtpdataerror-554-520-on-my-outlook-account/c9cd69cc-4ad0-4c30-b96f-67fc533b0603
+        
+        try:
+            server.sendmail(self.EMAIL_FROM, self.EMAIL_TO, msg.as_string())
+            server.quit()        
+        except Exception as error:
+            err = traceback.format_exc()
+            print(' ================================ sendmail traceback START ================================ ')
+            print(err)
+            print(' ================================ sendmail traceback END ================================ ')
+            return f'[{basefn}] email error : {err}.'
+        
+        return f'[{basefn}] email complete.'
+    
+    # ======== email ========
+    def email(self, filename):
+        return self.send_email(filename)
+
+    # ======================
+    #       exit 
+    # ======================
+    def exit(self):
+        quit()
+
